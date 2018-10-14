2be58ec28d27f784b3953d184d64e46f412f3347
Kirill <nosolution01@gmail.com>
2018-10-14 22:58:19 +0400

    Initial commit.

---
+++ src/CalculatingEngine.java
@@ -0,0 +1,22 @@
+/*
+ * CalculatingEngine class
+ * Contains main functionality for calcultor such as operation methods, etc.
+ * EPAM Java training for automation QA
+ * Author: Kirill Kuzin
+ * Lesson 1 Task 3
+ * */
+
+public class CalculatingEngine {
+    public static void doSum() {
+
+    }
+    public static void doDiff() {
+
+    }
+    public static void doMutliply(){
+
+    }
+    public static void doDivide(){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Main class
+ * EPAM Java training for automation QA
+ * Author: Kirill Kuzin
+ * Lesson 1 Task 3
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
